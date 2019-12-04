e7c287fdfa70b50098a9e8a93b90de3a38eae4a6
CarlosAguilar22 <CarlosAguilar22@users.noreply.github.com>
2019-12-03 21:59:48 -0600

    Anexinet TechnicalTest
    Anexinet C#/.NET Technical Test

---
+++ TechnicalTest/TechnicalTest/ExerciseFunctions.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTest
+{
+
+    public sealed class CartesianPoint
+    {
+		public int X {get; set;}
+		public int Y {get; set;}
+	}
+
+    internal static class Brackets
+    {
+        static Dictionary<char, char[]> dictValidBrackets;
+
+        /// <summary>
+        /// Constructor. 
+        /// </summary>
+        static Brackets()
+        {
+            dictValidBrackets = new Dictionary<char, char[]>();
+            dictValidBrackets.Add('(', new char[] { ')', '[', '('});
+            dictValidBrackets.Add(')', new char[] { '(', ')', '['});
+            dictValidBrackets.Add('[', new char[] { ']'});
+            dictValidBrackets.Add(']', new char[] { '[', ')','(' });
+        }
+
+        internal static bool isValidBracketChar(string bracketsString)
+        {
+            int validCharactersCount = bracketsString.Count(x => x == '(') +
+                                       bracketsString.Count(x => x == ')') +
+                                       bracketsString.Count(x => x == '[') +
+                                       bracketsString.Count(x => x == ']');
+            
+            if (validCharactersCount == bracketsString.Length)
+                return true;
+
+            return false;
+        }
+
+        internal static bool IsValidBracketsCombination(char currentChar, char nextChar)
+        {
+            char[] validBrackets = dictValidBrackets[currentChar];
+            if (validBrackets.Contains(nextChar))
+                return true;
+
+            return false;
+        }
+
+        internal static bool IsValidOpenClosedNumberBrackets(string bracketsString)
+        {
+            int openParenthesis = bracketsString.Count(x=> x== '(');
+            int closeParenthesis = bracketsString.Count(x => x == ')');
+            int openBrackets = bracketsString.Count(x => x == '[');
+            int closeBrackets = bracketsString.Count(x => x == ']');
+
+            if ((openParenthesis == closeParenthesis) && (openBrackets == closeBrackets))
+                return true;
+
+            return false;
+        }
+    }
+
+    public class ExerciseFunctions
+    {
+        /// <summary>
+        /// Function that determines if a date belongs to a leap year
+        /// </summary>
+        /// <param name="strLeapyear"></param>
+        /// <returns></returns>
+        public bool leapYear(string strLeapyear)
+        {
+            DateTime dtLeapYear = new DateTime();
+            bool success = DateTime.TryParse(strLeapyear, out dtLeapYear);
+            if (success)
+            {
+                int year = dtLeapYear.Year;
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                throw new Exception();
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Function that gets the common characteres of 2 strings        
+        /// /// </summary>
+        /// <param name="firstString"></param>
+        /// <param name="secondString"></param>
+        /// <returns></returns>
+        public Char[] repeatedStrings(string firstString, string secondString)
+        {
+            // Validate if the first string has a repeated character
+            foreach (char currentChar in firstString.ToCharArray())
+            {
+                int numberOcurrences = firstString.Count(x => x == currentChar);
+                if (numberOcurrences > 1)
+                    throw new Exception("The string provided as first string has next repeated character: " + currentChar + ", please provide a valid one");
+            }
+
+            // Validate if the second string has a repeated character
+            foreach (char currentChar in secondString.ToCharArray())
+            {
+                int numberOcurrences = secondString.Count(x => x == currentChar);
+                if (numberOcurrences > 1)
+                    throw new Exception("The string provided as second string has next repeated character: " + currentChar + ", please provide a valid one");
+            }
+
+            Char[] charArray = firstString.Intersect(secondString).ToArray();           
+
+            return charArray;
+        }
+
+        /// <summary>
+        /// Function that returns the compressing string from other one
+        /// </summary>
+        /// <param name="originalString"></param>
+        /// <returns></returns>
+        public string compressedString(string originalString)
+        {
+            String compressedString = String.Empty;
+
+            // Validate if the first string has a repeated character
+            foreach (char currentChar in originalString.ToCharArray())
+            {
+                int numberOcurrences = originalString.Count(x => x == currentChar);
+                if(!compressedString.Contains(currentChar))
+                    compressedString += currentChar + numberOcurrences.ToString();
+            }
+
+            return compressedString.Length < originalString.Length ? compressedString : originalString;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bracketsString"></param>
+        /// <returns></returns>
+        public bool closedBrackets(String bracketsString)
+        {
+            // Must be at least 2 characters to evaluate and its length must be divisible by 2
+            if (bracketsString.Length == 1 || !(bracketsString.Length % 2 == 0))
+                return false;
+
+            // Verify that all characters os the string are valid ones
+            if (!Brackets.isValidBracketChar(bracketsString))
+                return false;
+
+            Char[] bracketsArrayChar = bracketsString.ToCharArray();
+
+            // Compare the current posicion with the next one
+            for (int pos = 0; pos < bracketsString.Length - 1; pos++)
+            {
+                char currentChar = bracketsArrayChar[pos];
+                char nextChar = bracketsArrayChar[pos + 1];
+                if (!Brackets.IsValidBracketsCombination(currentChar, nextChar))
+                    return false;
+            }
+
+            // If everithing is fine so far, compare that the number of open and closed brackets is the same
+            if (Brackets.IsValidOpenClosedNumberBrackets(bracketsString))
+                return true;
+
+            return false;            
+        }
+
+        /// <summary>
+        /// Function that returns the smallest bounding area according with the given Cartesian Points
+        /// </summary>
+        /// <param name="lstCartesianPoints"></param>
+        /// <returns></returns>
+        public int smallestArea(List<CartesianPoint> lstCartesianPoints)
+        {            
+            int smallestXpoint=0, biggestXpoint=0, smallestYpoint=0, biggestYpoint=0;
+			
+            foreach (CartesianPoint cartesianPoint in lstCartesianPoints)
+            {
+                if (cartesianPoint.X < smallestXpoint)
+					smallestXpoint = cartesianPoint.X;
+
+               if (cartesianPoint.X > biggestXpoint)
+                    biggestXpoint = cartesianPoint.X;
+
+                if (cartesianPoint.Y < smallestYpoint)
+					smallestYpoint = cartesianPoint.Y;
+
+               if (cartesianPoint.Y > biggestYpoint)
+                    biggestYpoint = cartesianPoint.Y;											
+            }
+
+            return ((Math.Abs(smallestXpoint) + biggestXpoint) * (Math.Abs(smallestYpoint) + biggestYpoint));
+        }
+    }
+}
---
+++ TechnicalTest/TechnicalTest/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1.- Leap year function.");
+            Console.WriteLine("2.- Repeated strings.");
+            Console.WriteLine("3.- String compression.");
+            Console.WriteLine("4.- Closed Brackets.");
+            Console.WriteLine("5.- Bounding box area.");
+            string selectedoption = Console.ReadLine();
+            int intselectedoption = 0;
+            bool success = Int32.TryParse(selectedoption, out intselectedoption);
+            // Verify if the user's option is valid
+            if (success)
+            {
+                if (intselectedoption >= 1 && intselectedoption <= 5)
+                {
+                    ExerciseFunctions ef = new ExerciseFunctions();
+
+                    switch (intselectedoption)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Please privides a valid date:");
+                                string stringDate = Console.ReadLine();
+                                try
+                                {
+                                    bool isLeapYear = ef.leapYear(stringDate);
+                                    if (isLeapYear)
+                                        Console.WriteLine("The year provided is a Leap Year");
+                                    else
+                                        Console.WriteLine("The year provided is NOT a Leap Year");
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("The string provided is not a valid date");
+                                }
+                                Console.ReadLine();
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Please provide 2 strings without duplicate characteres:");
+                                Console.WriteLine("Please input the first string:");
+                                string firstString = Console.ReadLine();
+                                Console.WriteLine("Please input the second string:");
+                                string secondString = Console.ReadLine();
+                                try
+                                {
+                                    Char[] repeatedCharacters = ef.repeatedStrings(firstString, secondString);
+                                    Console.WriteLine("These are the repeated characters found in both provided scripts: ");
+                                    foreach (Char currentCharacter in repeatedCharacters)
+                                    {
+                                        Console.Write(currentCharacter + ",");
+                                    }                                    
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+                                Console.ReadLine();
+                                break;
+                            }
+                        case 3:
+                            {                                
+                                Console.WriteLine("Please input the original string:");
+                                string originalString = Console.ReadLine();
+                                String compressedString = ef.compressedString(originalString);
+                                Console.WriteLine("This is the compressed string: " + compressedString);
+                                Console.ReadLine();
+                                break;
+                            }
+                        case 4:
+                            {
+                                Console.WriteLine("Please provide the string with brackets:");
+                                string bracketsString = Console.ReadLine();
+                                try
+                                {
+                                    bool isValid = ef.closedBrackets(bracketsString);
+                                    if(isValid)
+                                        Console.WriteLine("The string provided has brackets properly closed");
+                                    else
+                                        Console.WriteLine("The string provided has NOT brackets properly closed");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+                                Console.ReadLine();
+                                break;
+                            }
+                        case 5:
+                            {
+                                Console.WriteLine("Please privide how many cartesian points you will provide:");
+                                string strCartesianPoints = Console.ReadLine();
+								int cartesianPoints = 0;
+								bool success2 = Int32.TryParse(strCartesianPoints, out cartesianPoints);
+								if(success2)
+								{
+									try
+									{
+										List<CartesianPoint> lstCartesianPoints = new List<CartesianPoint>();
+										Console.WriteLine("Please provide the cartesian points separate by comma.");
+										for(int x=0; x<cartesianPoints;x++)
+										{
+											Console.WriteLine("Please input the cartesian point number: " + x);
+											string strCP = Console.ReadLine();										
+											String[] arrayCartesianPoint = strCP.Split(',');
+											CartesianPoint cartesianPoint = new CartesianPoint();
+											cartesianPoint.X = Int32.Parse(arrayCartesianPoint[0]);
+											cartesianPoint.Y = Int32.Parse(arrayCartesianPoint[1]);
+											lstCartesianPoints.Add(cartesianPoint);
+										}										
+										int smallestArea = ef.smallestArea(lstCartesianPoints);
+										Console.WriteLine("The smallest bounding box area of the provided cartesian points is: " + smallestArea);
+									}
+									catch (Exception ex)
+									{
+										Console.WriteLine(ex.Message);
+									}
+								}
+								else
+									Console.WriteLine("Please input a valid number of cartesian points:");
+								
+                                Console.ReadLine();
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The input option is valid, please try again.");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("The input option is not a number, please try again.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
